de93f3389901e44412420ba396b634dcb44331d1
Sara Alhajri <93153872+ssh218@users.noreply.github.com>
2021-12-09 13:03:52 +0300

    Update models.py

--- poll_project/poll/models.py
+++ poll_project/poll/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=100)
+    question = models.CharField(max_length=100)
+    active_until=models.DateTimeField(auto_now=True)
+    status= models.DateTimeField(auto_now=True)
